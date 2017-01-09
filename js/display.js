4a2e68d9a07bc53f5e2090a8dbd79a7606c35504
Siddharth Chopra <sid6327@gmail.com>
2017-01-09 17:59:44 +0530

    Completely new display with only a single buffer for all pixels for 60fps gameplay.

--- js/display.js
+++ js/display.js
@@ -1,4 +1,4 @@
-var gl;	// Global variable for WebGL Features.
+var gl;
 
 function start () {
 	var canvas = document.getElementById('Display');
@@ -10,44 +10,25 @@ function start () {
 	initDisplay();
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.enable(gl.DEPTH_TEST);
-	drawScene();
+	gl.enable(gl.CULL_FACE);
 }
 
 function initGL (canvas) {
-	try {
-		gl = canvas.getContext("webgl");
-		gl.viewportHeight = canvas.height;
-		gl.viewportWidth = canvas.width;
-	} catch (e) {
-		console.log(e);
-	}
-	if (!gl) {
-		console.log("Not supported");
-	}
+	gl = canvas.getContext("webgl");
+	gl.viewportHeight = canvas.height;
+	gl.viewportWidth = canvas.width;
 }
 
-function getShader (gl, id) {
+function getShader (id) {
 	var shaderScript = document.getElementById(id);
-	if (!shaderScript) {
-		return null;
-	}
-	var str = "";
-	var k = shaderScript.firstChild;
-	while (k) {
-		if (k.nodeType == 3) {
-			str += k.textContent;
-		}
-		k = k.nextSibling;
-	}
+	var script = shaderScript.textContent;
 	var shader;
-	if (shaderScript.type == "x-shader/x-fragment") {
-		shader = gl.createShader(gl.FRAGMENT_SHADER);
-	} else if (shaderScript.type == "x-shader/x-vertex") {
+	if (shaderScript.type == "vertex") {
 		shader = gl.createShader(gl.VERTEX_SHADER);
 	} else {
-		return null;
+		shader = gl.createShader(gl.FRAGMENT_SHADER);
 	}
-	gl.shaderSource(shader, str);
+	gl.shaderSource(shader, script);
 	gl.compileShader(shader);
 	if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
 		console.log(gl.getShaderInfoLog(shader));
@@ -58,89 +39,103 @@ function getShader (gl, id) {
 
 var shaderProgram;
 function initShaders () {
-	// Get shaders written in index.html
-	var fragmentShader = getShader(gl, "shader-fs");
-	var vertexShader = getShader(gl, "shader-vs");
+	var fragmentShader = getShader("fragment");
+	var vertexShader = getShader("vertex");
 	shaderProgram = gl.createProgram();
-
-	// Attach shaders to global gl
 	gl.attachShader(shaderProgram, vertexShader);
 	gl.attachShader(shaderProgram, fragmentShader);
 	gl.linkProgram(shaderProgram);
-	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-		console.log("Could not initialise shaders.");
-	}
-
-	// Activate shader
 	gl.useProgram(shaderProgram);
 	shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "vertexPositions");
 	gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-	shaderProgram.colorVector = gl.getUniformLocation(shaderProgram, "colorVector");
+	shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "vertexColors");
+	gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
+	shaderProgram.colorUniform = gl.getUniformLocation(shaderProgram, "uColor");
 }
 
-var pixelPositionBuffer = [];
-var pixelPositionItemSize = 3;
-var pixelPositionNumberOfItems = 4;
+// Vertex Position Attribute
+var pixelPositionBuffer;
+var pixelPositionItemSize = 2;
+var pixelPositionNumberOfItems = 4 * 32 * 64;
+
+// Vertex Color Attribute
+var pixelColorBuffer;
+var pixelColorItemSize = 2;
+var pixelColorNumberOfItems = 4 * 32 * 64;
 
 function initBuffers () {
-	for (var i = 0; i < 32; i++) {
-		pixelPositionBuffer.push([]);
-	}
+	pixelPositionBuffer = gl.createBuffer();
+	pixelColorBuffer = gl.createBuffer();
 	var vertices = [];
 	for (var i = 0; i < 32; i++) {
 		for (var j = 0; j < 64; j++) {
-			pixelPositionBuffer[i].push(gl.createBuffer());
-			gl.bindBuffer(gl.ARRAY_BUFFER, pixelPositionBuffer[i][j]);
-			// Since width is to be 64 pixels, we will set offset as 1/64 * 2 for width and height is 32 pixels so it's offset will be 1/64 * 4
-			vertices = [
-				-1 + 0.03125 + 0.03125 * j, 1 - 0.0 - 0.0625 * i, 0.0,
-				-1 + 0.0 + 0.03125 * j, 1 - 0.0 - 0.0625 * i, 0.0,
-				-1 + 0.03125 + 0.03125 * j, 1 - 0.0625 - 0.0625 * i, 0.0,
-				-1 + 0.0 + 0.03125 * j, 1 - 0.0625 - 0.0625 * i, 0.0
-			];
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+			vertices = vertices.concat([
+				-1 + 0.03125 + 0.03125 * j, 1 - 0.0 - 0.0625 * i,
+				-1 + 0.0 + 0.03125 * j, 1 - 0.0 - 0.0625 * i,
+				-1 + 0.03125 + 0.03125 * j, 1 - 0.0625 - 0.0625 * i,
+				-1 + 0.0 + 0.03125 * j, 1 - 0.0625 - 0.0625 * i
+			]);
 		}
 	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, pixelPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
-// A 2-D array to represent the state of the display
-var display = [];
+var displayBuffer = new Float32Array(32 * 64 * 8);
+var displayMatrix = new Uint8Array(32 * 64);
+
 function initDisplay () {
 	for (var i = 0; i < 32; i++) {
-		display.push([]);
 		for (var j = 0; j < 64; j++) {
-			display[i].push(0);
+			displayMatrix[i * 64 + j]
 		}
 	}
-} 
-
-// RGBA values for pixel on and off
-var pixelOn = [0.0, 1.0, 0.231, 1.0];
-var pixelOff = [0.0, 0.0, 0.0, 1.0];
+	for (var i = 0; i < displayBuffer.length; i++) {
+		displayBuffer[i] = 0.0;
+	}
+	gl.bindBuffer(gl.ARRAY_BUFFER, pixelPositionBuffer);
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, pixelPositionItemSize, gl.FLOAT, false, 0, 0);
+}
 
 function drawScene () {
 	gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
 	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-	for (var i = 0; i < 32; i++) {
-		for (var j = 0; j < 64; j++) {
-			gl.bindBuffer(gl.ARRAY_BUFFER, pixelPositionBuffer[i][j]);
-			gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, pixelPositionItemSize, gl.FLOAT, false, 0, 0);			
-			if (display[i][j]) {
-				var colorVector = new Float32Array(pixelOn);
-			}
-			else {
-				var colorVector = new Float32Array(pixelOff);
-			}
-			gl.uniform4fv(shaderProgram.colorVector, colorVector);
-			gl.drawArrays(gl.TRIANGLE_STRIP, 0, pixelPositionNumberOfItems);
-		}
+	gl.bindBuffer(gl.ARRAY_BUFFER, pixelColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, displayBuffer, gl.STATIC_DRAW);
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, pixelColorItemSize, gl.FLOAT, false, 0, 0);
+
+	gl.drawArrays(gl.TRIANGLE_STRIP, 0, pixelPositionNumberOfItems);
+}
+
+function setPixel (posX, posY) {
+	if (posX > 31) {
+		posX %= 31;
+	}
+	if (posY > 63) {
+		posY %= 63;
+	}
+	var index = posX * 64 + posY;
+	if (displayMatrix[index]) {
+		displayMatrix[index] = 0;
+		turnPixelOff(posX, posY);
+		return true;
+	} else {
+		displayMatrix[index] = 1;
+		turnPixelOn(posX, posY);
+		return false;
 	}
 }
 
-function clear () {
-	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+function turnPixelOn (posX, posY) {
+	var index = (posX * 64 * 8) + (posY * 8);
+	displayBuffer[index + 0] = displayBuffer[index + 2] = displayBuffer[index + 4] = displayBuffer[index + 6] = 1.0; 
+	displayBuffer[index + 1] = displayBuffer[index + 3] = displayBuffer[index + 5] = displayBuffer[index + 7] = 0.231;
+}
+
+function turnPixelOff (posX, posY) {
+	var index = (posX * 64 * 8) + (posY * 8);
+	displayBuffer[index + 0] = displayBuffer[index + 2] = displayBuffer[index + 4] = displayBuffer[index + 6] = displayBuffer[index + 1] = displayBuffer[index + 3] = displayBuffer[index + 5] = displayBuffer[index + 7] = 0.0;
 }
 
 function drawSprite (opCode, x, y) {
@@ -151,28 +146,11 @@ function drawSprite (opCode, x, y) {
 		sprite = CHIP8.memory[CHIP8.i + i];
 		for (var j = 0; j < 8; j++) {
 			if (sprite & 0x0080) {
-				if (colorPixel(posX + i, posY + j)) {
+				if (setPixel(posX + i, posY + j)) {
 					CHIP8.v[0x000f] = 1;
 				}
 			}
 			sprite = sprite << 1;
 		}
-	}
-}
-
-function colorPixel (posX, posY) {
-	if (posX > 31) {
-		posX %= 31;
-	}
-	if (posY > 63) {
-		posY %= 63;
-	}
-	// console.log("posx = " + posX + ", posy = " + posY);
-	if (display[posX][posY]) {
-		display[posX][posY] = 0;
-		return true;
-	} else {
-		display[posX][posY] = 1;
-		return false;
 	}
 }
